ca5ae1bcb9aa23cc27f27881580498073b64e4f3
Zohaib <zohaibmanzoor1700@gmail.com>
2021-11-01 13:50:38 +0000

    completed

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 //1
-function greatestOfTwo(){
-  
-}
+const greatestOfTwo = (int1, int2) => {
+    if (int1 > int2) {
+        return int1;
+    }
+    else {
+        return int2;
+    }
+};
+// console.log(greatestOfTwo( -1, 1))
 
 //2
-function greatestOfThree(){
-  
-}
+const greatestOfThree = (int1, int2, int3) => {
+    if (int1 > int2 && int1 > int3) {
+        return int1;
+    }
+    else if (int2 > int1 && int2 > int3) {
+        return int2;
+    }
+    else {
+        return int3;
+    }
+};
+// console.log(greatestOfThree(-3, -2, -11))
 
 //3
-function greatestOfFour(){
-  
-}
-
+const greatestOfFour = (int1, int2, int3, int4) => {
+    if (int1 > int2 && int1 > int3 && int1 > int4) {
+        return int1;
+    }
+    else if (int2 > int1 && int2 > int3 && int2 > int4) {
+        return int2;
+    }
+    else if (int3 > int1 && int3 > int2 && int3 > int4) {
+        return int3;
+    }
+    else {
+        return int4;
+    }
+};
+// console.log(greatestOfFour(10, 2, 6, 11))
 
 //4
-function leastOfFour(){
-  
-}
+const leastOfFour = (int1, int2, int3, int4) => {
+  if (int1 < int2 && int1 < int3 && int1 < int4) {
+        return int1;
+    } 
+    else if (int2 < int1 && int2 < int3 && int2 < int4) {
+        return int2;
+    }
+    else if (int3 < int1 && int3 < int2 && int3 < int4) {
+        return int3;
+    }
+    else {
+        return int4;
+    }
+};
+// console.log(leastOfFour(10, 3, 6, 11))
